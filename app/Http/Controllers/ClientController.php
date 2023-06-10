3f48c606263130ef0c5d28e1bb73cb2dee695e22
Leandro Marcos <leandromarcosdedeus@gmail.com>
2023-06-10 10:20:16 -0300

    chore: Add new method ClientController

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use App\Validacao\Validacao;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class ClientController extends Controller
+{
+    public function getClients(){
+        return response()->json(Client::all(), 200);
+    }
+    public function getClientsById($id){
+        $client = Client::find($id);
+
+        if(is_null($client)){
+            return response()->json(['message:'=>'Cliente não encontrado'], 404);
+        }
+        return response()->json($client::find($id));
+    }
+    public function addClient(Request $request){
+        
+        $teste = new Validacao;
+        //nome, cpf, 
+        if($teste->validarClient($request) == true){
+            $client = Client::create($request->all());
+            return response($client, 201);
+        }
+        else{
+            return response()->json(['message'=>'Cadastro recusado']);
+        }
+    }
+    public function updateClient(Request $request, $id){
+        $client = Client::find($id);
+        if(is_null($client)){
+            return response()->json(['message:'=>'Cliente não encontrado'], 404);
+        }
+        $client->update($request->all());
+        return response($client, 200);
+    }
+    public function deleteClient(Request $request, $id){
+        $client = Client::find($id);
+        if(is_null($client)){
+            return response()->json(['message:'=>'Cliente não encontrado'], 404);          
+        }
+        $client->delete();
+        return response()->json(null, 204);
+    }
+
+    public function mostFrequent2(){
+        $data = Client::all();
+        $result = collect();
+        foreach ($data->groupBy('cidade') as $cidade=>$values) {
+                $result->push([
+                    'cidade'=> $cidade,
+                    'values'=> $values->count()
+                ]);
+        }
+        return $result;
+    }
+    public function mostFrequent(){
+        /*
+        $mostFrequent = DB::table('clients')->select('cidade',
+        DB::raw('COUNT(*) as total'))->groupBy('cidade')->orderByDesc('total')->first();
+        return $mostFrequent;*/
+        $data = Client::all();
+        $result = collect();
+        foreach ($data->groupBy('cidade') as $cidade=>$values) {
+                $result->push([
+                    'cidade'=> $cidade,
+                    'values'=> $values->count()
+                ]);
+        }
+        return $result;
+    }
+}
---
+++ app/Validacao/Validacao.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Validacao;
+
+class Validacao{
+    public function validarClient($request) {
+        $teste = new Validacao;
+        if(!$teste->validarNome($request->nome) ||
+           !$teste->validarCPF($request->CPF) ||
+           !$teste->validarApelido($request->apelido) ||
+           !$teste->validarCidade($request->cidade) ||
+           !$teste->validarTime($request->time) ||
+           !$teste->validarHobbie($request->hobbie)) {
+            return false;
+        }
+    
+        return true;
+    }
+    
+    public function validarCPF($cpf) {
+        $cpf = preg_replace('/[^0-9]/', '', $cpf);
+    
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+    
+        $digito1 = 0;
+        $digito2 = 0;
+    
+        for ($i = 0; $i < 9; $i++) {
+            $digito1 += $cpf[$i] * (10 - $i);
+            $digito2 += $cpf[$i] * (11 - $i);
+        }
+    
+        $resto1 = ($digito1 % 11) < 2 ? 0 : 11 - ($digito1 % 11);
+        $resto2 = ($digito2 + ($resto1 * 2)) % 11 < 2 ? 0 : 11 - (($digito2 + ($resto1 * 2)) % 11);
+    
+        if ($cpf[9] != $resto1 || $cpf[10] != $resto2) {
+            return false;
+        } 
+        return true;
+    }
+    
+    public static function validarNome($nome) {
+        $pattern = '/[\'^£$%&*()}{@#~?><>,|=_+¬-]/';
+        $patternNumber = '0123456789';
+
+        if(preg_match($pattern, $nome) || $nome == null){
+            return false;
+        }
+        return true;
+    }
+    public static function validarApelido($apelido){
+        if(is_null($apelido)){
+            return false;   
+        }
+        return true;
+    }
+    public static function validarHobbie($hobbie){
+        if(is_null($hobbie)){
+            return false;   
+        }
+        return true; 
+    }
+    public static function validarTime($time){
+        if(is_null($time)){
+            return false;   
+        }
+        return true; 
+    }
+    public static function validarCidade($cidade){
+        if($cidade != null){
+            return true;
+        }
+        return false; 
+    }
+    
+}
